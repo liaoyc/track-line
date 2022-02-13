df0cae91796abb8b0783a2aa779497c723d6ad0f
liaoyc <liaoyuncong@gmail.com>
2022-02-13 12:24:35 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+startbit.startbit_Init()
+startbit.setServo(startbit.startbit_servorange.range1, 1, 90, 500)
+startbit.setServo(startbit.startbit_servorange.range1, 2, 90, 500)
+basic.pause(500)
 basic.forever(function () {
-	
+    if (startbit.startbit_line_followers(startbit.startbit_LineFollowerSensors.S2, startbit.startbit_LineColor.White) && startbit.startbit_line_followers(startbit.startbit_LineFollowerSensors.S3, startbit.startbit_LineColor.Black)) {
+        startbit.startbit_setMotorSpeed(-90, 90)
+    } else if (startbit.startbit_line_followers(startbit.startbit_LineFollowerSensors.S2, startbit.startbit_LineColor.Black) && startbit.startbit_line_followers(startbit.startbit_LineFollowerSensors.S3, startbit.startbit_LineColor.White)) {
+        startbit.startbit_setMotorSpeed(90, -90)
+    } else {
+        startbit.startbit_setMotorSpeed(90, 90)
+    }
 })
